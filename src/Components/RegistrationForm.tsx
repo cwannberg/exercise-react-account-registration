b6abe3ae3dd79469b9043022d9ca6f483740d737
Cilla <ceciliawannberg@gmail.com>
2025-08-28 15:46:10 +0200

    validation added

--- src/Components/RegistrationForm.tsx
+++ src/Components/RegistrationForm.tsx
@@ -1,6 +1,14 @@
 import { useState, type ChangeEvent, type ReactElement } from "react";
 import '../assets/registrationForm.css';
 
+const isPasswordValid = (password: string, confirmPassword: string): boolean => {
+   if(password.length >= 8 && password === confirmPassword){
+        console.log("Du är registrerad");
+        return true;
+   }
+   return false;
+}
+
 export function RegistrationForm() : ReactElement {
   const [formData, setFormData] = useState({
     name: "name",
@@ -11,11 +19,11 @@ export function RegistrationForm() : ReactElement {
   });
 
     const handleSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
-      e.preventDefault(); 
-      console.log("Form submitted:", formData);
+      e.preventDefault();
+      console.log("Du är registrerad");
     }
   
-    const handleChange = (e: ChangeEvent<HTMLInputElement>): void =>{
+    const handleChange = (e: ChangeEvent<HTMLInputElement>): void =>{ 
         const {name, value} = e.target;
         setFormData((prev) =>({
             ...prev, [name]: value,
@@ -33,6 +41,7 @@ export function RegistrationForm() : ReactElement {
               name="name"
               value={formData.name}
               onChange={handleChange}
+              required
             />  
             </label>
           <label>
@@ -42,6 +51,7 @@ export function RegistrationForm() : ReactElement {
               name="username"
               value={formData.username}
               onChange={handleChange}
+              required
             />
             </label>
           <label>
@@ -51,6 +61,7 @@ export function RegistrationForm() : ReactElement {
               name="email"
               value={formData.email}
               onChange={handleChange}
+              required
             />
             </label>
           <label>
@@ -60,6 +71,7 @@ export function RegistrationForm() : ReactElement {
               name="password"
               value={formData.password}
               onChange={handleChange}
+              required
             />
             </label>
           <label>
@@ -69,9 +81,12 @@ export function RegistrationForm() : ReactElement {
               name="confirmPassword"
               value={formData.confirmPassword}
               onChange={handleChange}
+              required
             />
             </label>
-          <button type="submit">Sign up</button>
+          <button 
+            type="submit"
+            disabled={!isPasswordValid(formData.password, formData.confirmPassword)}>Sign up</button>
         </form>
       </section>
     )
